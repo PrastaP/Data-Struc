6a61bf9026443a48391580cf3a2f978f5f73ad80
PrastaP <152815672+PrastaP@users.noreply.github.com>
2024-02-22 18:40:03 -0500

    Create Wedding.java

---
+++ Wedding.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+
+// Wedding.java
+public class Wedding {
+    private Couple couple;
+    private LocalDate weddingDate;
+    private String location;
+
+    public Wedding(Couple couple, LocalDate weddingDate, String location) {
+        this.couple = couple;
+        this.weddingDate = weddingDate;
+        this.location = location;
+    }
+    public Couple getCouple() {
+        return couple;
+    }
+    public LocalDate getWeddingDate() {
+        return weddingDate;
+    }
+    public String getLocation() {
+        return location;
+    }
+}
