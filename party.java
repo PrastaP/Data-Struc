11a3ebdec67ac129ff8e7ae76d33cbe2cad8e094
PrastaP <152815672+PrastaP@users.noreply.github.com>
2024-02-22 20:11:14 -0500

    Create party.java

---
+++ party.java
@@ -0,0 +1,15 @@
+// Party.java
+import java.util.Scanner;
+
+public class Party {
+    private int guests;
+    public void setGuests(int numGuests) {
+        guests = numGuests;
+    }
+    public int getGuests() {
+        return guests;
+    }
+    public void displayInvitation() {
+        System.out.println("come to my party!");
+    }
+}
